79062d56593394d9ceef926a6fb5f6a2086b08a6
J. Caleb Travers <caleb@popwallet.com>
2018-02-23 16:11:34 -0600

    Commit last changes

--- src/main/java/com/calebtravers/mealplannerreact/RecipeRepository.java
+++ src/main/java/com/calebtravers/mealplannerreact/RecipeRepository.java
@@ -1,8 +1,9 @@
 package com.calebtravers.mealplannerreact;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+public interface RecipeRepository extends PagingAndSortingRepository<Recipe, Long> {
 
 
 }
